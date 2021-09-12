9652e4fbe232fabda1c6c2665d5b9b3330cffb72
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-12 12:25:28 +0100

    I scanned the files in 'com.sf.test' with an annotation called @ComponentScan

--- src/main/java/com/sf/test/ApplicationConfiguration.java
+++ src/main/java/com/sf/test/ApplicationConfiguration.java
@@ -1,4 +1,16 @@
 package com.sf.test;
 
-public class ApplicationConfiguration {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@ComponentScan(basePackages = "com.sf.test")
+public class ApplicationConfiguration extends WebMvcConfigurationSupport {
+	
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		super.addResourceHandlers(registry);
+	}
 }
