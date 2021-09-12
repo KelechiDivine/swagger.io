93a0254f0687d1428938ed912953dedd322cb12d
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-12 12:10:47 +0100

    new commit

---
+++ src/main/java/com/sf/test/ApplicationInitializer.java
@@ -0,0 +1,16 @@
+package com.sf.test;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		//bootstrap the dispatcher servlet
+		
+	
+	}
+}
---
+++ src/test/java/com/sf/swagger/SwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sf.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
