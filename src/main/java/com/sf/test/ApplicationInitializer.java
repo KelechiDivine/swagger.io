ba65a7b95f053a623e45d226ec248b0a5ef08558
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-12 12:14:54 +0100

    added the dispatcherServle()

--- src/main/java/com/sf/test/ApplicationInitializer.java
+++ src/main/java/com/sf/test/ApplicationInitializer.java
@@ -1,16 +1,22 @@
 package com.sf.test;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class ApplicationInitializer implements WebApplicationInitializer {
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		
 		//bootstrap the dispatcher servlet
+		AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
 		
+		ServletRegistration.Dynamic dynamic = servletContext.addServlet("swaggger",
+				new DispatcherServlet(webApplicationContext));
 	
 	}
 }
