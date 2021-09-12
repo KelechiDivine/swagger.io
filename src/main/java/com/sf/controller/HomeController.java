be2ea12eec74db8efad429e54597ef6dec6bd059
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-09-12 13:02:15 +0100

    I added mysql maven dependency

--- src/main/java/com/sf/controller/HomeController.java
+++ src/main/java/com/sf/controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.sf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+
 public class HomeController {
+	
+	@GetMapping("/home")
+	public String goHome(){
+		System.out.println("in home controller...");
+		return "index";
+	}
 }
